d8374a030bfe973e67ffbe28f5d86d2a1bd52a8a
Josue87 <j.encinar.87@gmail.com>
2020-10-02 13:33:18 +0200

    improvement of regex thanks to Ricardo

--- regex/injection.py
+++ regex/injection.py
@@ -3,7 +3,6 @@ from regex._template import Template
 class Regex(Template):
 
     def __init__(self):
-        pattern_list = [r".*eval\(([^\"\' ]+)|(.+[+].+)\)\s*;", r".*exec\(([^\"\' ]+)|(.+[+].+)\)\s*;",
-                        r".*system\(([^\"\' ]+)|(.+[+].+)\)\s*;", r".+.command\([^\"\'\n]+\)\s*;",
-                        r".+.(command|executeCommand)\([^\"\'\n]+\)\s*;"]
+        pattern_list = [r"[^\w]eval\(([^\"\' \n]*)\)", r"[^\w]exec\(([^\"\' \n]*)\)",
+                        r"[^\w]system\(([^\"\' \n]*)\)", r"[^\w]command\(([^\"\' \n]*)\)"]
         super(Regex, self).__init__(pattern_list, "injection")
--- regex/xss.py
+++ regex/xss.py
@@ -3,5 +3,5 @@ from regex._template import Template
 class Regex(Template):
 
     def __init__(self):
-        pattern_list = [r".*write\(([^\"\'\n]+)|(.+[+].+)\)\s*;"]
+        pattern_list = [r"[\W]write\(([^,\"\'\n]+)\)", r'setJavaScriptEnabled(true)']
         super(Regex, self).__init__(pattern_list, "xss")
